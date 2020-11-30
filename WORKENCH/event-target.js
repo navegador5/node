0e69e43ee2ea73d507a193fc8c7307334cad62f4
dli <terryinzaghi@163.com>
2020-11-30 09:31:21 +0000

    bootsstrap/node

---
+++ WORKENCH/event-target.js
@@ -0,0 +1,24 @@
+[Exposed=(Window,Worker,AudioWorklet)]
+interface EventTarget {
+  constructor();
+
+  undefined addEventListener(DOMString type, EventListener? callback, optional (AddEventListenerOptions or boolean) options = {});
+  undefined removeEventListener(DOMString type, EventListener? callback, optional (EventListenerOptions or boolean) options = {});
+  boolean dispatchEvent(Event event);
+};
+
+callback interface EventListener {
+  undefined handleEvent(Event event);
+};
+
+dictionary EventListenerOptions {
+  boolean capture = false;
+};
+
+dictionary AddEventListenerOptions : EventListenerOptions {
+  boolean passive = false;
+  boolean once = false;
+};
+
+
+lib/internal/event_target.js
---
+++ WORKENCH/todo.js
@@ -0,0 +1,9 @@
+  node:internal/streams/readable:962
+    process.nextTick(resume_, stream, state);
+
+
+console -> 
+      window
+        js
+        html 
+        css
